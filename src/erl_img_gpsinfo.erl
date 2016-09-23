8274b8b2d033393d696d1e7efb87535804a53199
Wu Lei <mjollnir.ray@gmail.com>
2016-09-23 14:02:24 +0800

    fixed file encode & format: using utf-8 & unix

--- src/erl_img_gpsinfo.erl
+++ src/erl_img_gpsinfo.erl
@@ -1,8 +1,8 @@
-%%% coding: latin-1
+%%% coding: utf-8
 %%% File    : gpsinfo.erl
-%%% Author  : Davide Marquês <nesrait@gmail.com>
+%%% Author  : Davide MarquÃªs <nesrait@gmail.com>
 %%% Description : Utils for decoding Exif GPSInfo tags
-%%% Created :  20 Aug 2009 by Davide Marquês <nesrai@gmail.com>
+%%% Created :  20 Aug 2009 by Davide MarquÃªs <nesrai@gmail.com>
 
 -module(erl_img_gpsinfo).
 
